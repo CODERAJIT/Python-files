325650dca5a66aaffd0d736d4dc36361c8b6f8bf
AJIT KUMAR BEHERA <118212308+CODERAJIT@users.noreply.github.com>
2022-11-15 19:32:14 +0530

    Add files via upload

---
+++ loop/forloop.py
@@ -0,0 +1,29 @@
+'''
+for loop
+========
+
+syntax:
+     for variable in range(start,stop,step):
+
+              body of loop to be repeated
+
+
+variable is a counter variable.
+
+'''
+'''
+i=3
+r=i in range(1,10,1)
+print(r)
+
+e=11
+f=e in range(1,10,1)
+print(f)
+
+
+
+'''
+
+n=int(input("enter your number"))
+for i in range(1,n+1,1):
+    print(i)
