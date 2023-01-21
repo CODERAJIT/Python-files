de7ef22dc057e5f0f1d90cd96b853c4c3b14cc55
AJIT KUMAR BEHERA <118212308+CODERAJIT@users.noreply.github.com>
2023-01-21 20:58:09 +0530

    Add files via upload

---
+++ loop/while else _for else.py
@@ -0,0 +1,14 @@
+
+for i in range(1,5,1):
+    print("in for loop body",i)
+else:
+    print("in else part of loop")
+
+'''
+for i in range(1,5,1):
+    if i==3:
+        break
+    print("in for loop body",i)
+else:
+    print("in else part of loop")
+'''
