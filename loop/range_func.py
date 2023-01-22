f2c09081417f6886aabb7d1ccb3e8a923f0ed57a
AJIT KUMAR BEHERA <118212308+CODERAJIT@users.noreply.github.com>
2023-01-22 09:03:53 +0530

    Add files via upload

---
+++ loop/range_func.py
@@ -0,0 +1,53 @@
+'''
+range():This function generate list of numbers in a sequence.
+syntax:
+
+     range(start,stop,step)
+
+     start=> initialization
+     stop => conditon
+     step => increment/decrement [positive or negative]
+     
+'''
+
+
+#x=list(range(1,10,2))
+#print(x)
+
+'''
+x=list(range(2,20))
+print(x)
+
+x=list(range(10))
+print(x)
+
+
+'''
+x=list(range(15,5,-2))
+print(x)
+
+'''
+#int(input())
+x=list(range())#error as range() require at least one argument
+print(x)
+'''
+
+
+'''
+#int(input())
+x=list(range(20,2,-2))#negative number
+print(x)
+'''
+
+#No step and start
+'''
+#int(input())
+x=list(range(20))#default step is 0
+print(x)
+'''
+
+'''
+#int(input())
+x=list(range(2,20))#default step is 1
+print(x)
+'''
