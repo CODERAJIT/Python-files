bd42500068a8beb03145a2a8fe22bba9901b1f11
AJIT KUMAR BEHERA <118212308+CODERAJIT@users.noreply.github.com>
2023-01-24 18:20:11 +0530

    Add files via upload

---
+++ FUNCTIONS/arguments_summation.py
@@ -0,0 +1,25 @@
+'''
+Arguments:
+==========
+values or variable that are passed by the function call to the function
+definition or body are called as arguments.
+
+   Number of argument or        Number of variable used to
+   values or variable passed =  receive.
+
+'''
+def addition(x,y):
+    #print(x)
+    #print(y)
+    z=x+y
+    print("addition is :",z)
+
+
+addition(10,20)
+addition(200,40)
+#addition is : 30
+#addition is : 240
+'''
+10
+20
+'''
